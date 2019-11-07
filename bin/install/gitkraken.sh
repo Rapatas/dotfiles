27a6726cef411befb5837038dc2d81e04d2223af
Andy Rapatas <15367779+Rapatas@users.noreply.github.com>
2019-11-07 22:05:09 +0200

    Add templates

--- bin/install/gitkraken.sh
+++ bin/install/gitkraken.sh
@@ -2,5 +2,6 @@
 
 cd
 wget -O gitkraken_latest.deb "https://release.gitkraken.com/linux/gitkraken-amd64.deb"
-sudo gdebi gitkraken_latest.deb
+sudo dpkg -i gitkraken_latest.deb
+sudo apt install -f
 rm gitkraken_latest.deb
