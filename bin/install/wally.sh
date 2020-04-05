74a9e711b2777ad908628b023f1f8fd067717849
Andreas Rapatas <a.rapatas@amco.gr>
2020-04-05 13:48:45 +0300

    Add wally installer

---
+++ bin/install/wally.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+sudo apt install -y gtk+3.0 webkit2gtk-4.0 libusb-dev
+
+echo '
+# Teensy rules for the Ergodox EZ Original / Shine / Glow
+ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
+
+# STM32 rules for the Planck EZ Standard / Glow
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", \
+    MODE:="0666", \
+    SYMLINK+="stm32_dfu"
+' | sudo tee -a /etc/udev/rules.d/50-wally.rules
+
+wget -o ~/bin/wally https://configure.ergodox-ez.com/wally/linux
+chmod +x ~/bin/wally
