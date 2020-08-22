7f942e693172c9d1eef093f0cd2f396058ac2f23
Andy Rapatas <15367779+Rapatas@users.noreply.github.com>
2020-08-22 13:17:50 +0300

    Add some install scripts

---
+++ bin/install/docker.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+
+sudo apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+
+sudo curl \
+  -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
+  -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo curl \
+  -L https://raw.githubusercontent.com/docker/compose/1.26.2/contrib/completion/bash/docker-compose \
+  -o /etc/bash_completion.d/docker-compose
