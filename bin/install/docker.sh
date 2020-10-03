cc9ff19e1ed2ace5e8fb5513c1c68fff5e39033c
Andy Rapatas <15367779+Rapatas@users.noreply.github.com>
2020-10-03 19:09:01 +0300

    Doccker for arm

--- bin/install/docker.sh
+++ bin/install/docker.sh
@@ -13,19 +13,38 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo apt-key fingerprint 0EBFCD88
 
-sudo add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
+if [ "$(uname -m)" = "x86_64" ]; then 
+  sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+else 
+  sudo add-apt-repository \
+    "deb [arch=arm64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+fi
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 
-sudo curl \
-  -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
-  -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+if [ "$(uname -m)" = "x86_64" ]; then 
 
-sudo curl \
-  -L https://raw.githubusercontent.com/docker/compose/1.26.2/contrib/completion/bash/docker-compose \
-  -o /etc/bash_completion.d/docker-compose
+  sudo curl \
+    -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+
+  sudo curl \
+    -L https://raw.githubusercontent.com/docker/compose/1.26.2/contrib/completion/bash/docker-compose \
+    -o /etc/bash_completion.d/docker-compose
+
+else
+
+  sudo apt-get install -y libffi-dev libssl-dev python3 python3-pip python3-dev
+  sudo apt-get remove -y python-configparser
+
+  # ETA 22min.
+  sudo pip3 -v install docker-compose 
+
+fi
