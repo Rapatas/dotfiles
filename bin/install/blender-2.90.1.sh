7a49ce86f932803f47f8b85e311ca6cd011fb586
Andy Rapatas <15367779+Rapatas@users.noreply.github.com>
2020-11-08 10:59:33 +0200

    Add blender installer

---
+++ bin/install/blender-2.90.1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cd /dev/shm/
+wget https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.90/blender-2.90.1-linux64.tar.xz
+[ -d "/opt/blender-2.90.1-linux64" ] \
+  && sudo rm -rf /opt/blender-2.90.1-linux64
+sudo tar \
+  -xvf \
+  blender-2.90.1-linux64.tar.xz \
+  -C /opt/ 
+
+echo '[Desktop Entry]
+Name=Blender 2.90.1
+Comment=Open source 3D creation. Free to use for any purpose, forever.
+Encoding=UTF-8
+Exec=/opt/blender-2.90.1-linux64/blender
+TryExec=/opt/blender-2.90.1-linux64/blender
+Icon=/opt/blender-2.90.1-linux64/blender.svg
+StartupNotify=true
+Terminal=false
+Type=Application
+Version=1.0
+' | sudo tee /usr/share/applications/blender.desktop
+
+echo "application/x-blender=blender.desktop" \
+  | sudo tee -a /usr/share/applications/defaults.list
